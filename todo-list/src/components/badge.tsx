9c13754f14538ffb9fd58be356496e32b18d18cf
Brenobn <brenobragabn@gmail.com>
2025-06-29 12:42:33 -0300

    Importing cva and creanting a const called badgeVariants using cva to setting the styles

--- todo-list/src/components/badge.tsx
+++ todo-list/src/components/badge.tsx
@@ -1,5 +1,22 @@
 import type React from "react";
 import Text from "./text";
+import { cva, type VariantProps } from "class-variance-authority";
+
+export const badgeVariants = cva("inline-flex items-center justify-center rounded-full", {
+  variants: {
+    variant: {
+      primary: "bg-green-light",
+      secondary: "bg-pink-light"
+    },
+    size: {
+      sm: "py-0.5 px-2"
+    }
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "sm"
+  }
+});
 
 interface BadgeProps extends React.ComponentProps<"div"> {}
 
