ac17ce183fd2a555461fa74718d069b537ff18e8
Brenobn <brenobragabn@gmail.com>
2025-06-29 11:24:40 -0300

    Starting to build the file app.tsx importing Text and TrashIcon

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,7 +1,18 @@
+import Text from "./components/text";
+import TrashIcon from "./assets/icons/Trash.svg?react";
+
 export default function App() {
   return (
-    <>
-      Hello world!
-    </>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-1">
+        <Text variant="body-sm-bold" className="text-green-dark">       
+          Hello world!
+        </Text>
+      </div>
+
+      <div className="flex gap-1">
+        <TrashIcon />
+      </div>
+    </div>
   )
 }
