c953a95a5f7e70ce5df9c49bbc6506e519912e94
Lucas Veloso <lucas.g-external@intelltech.com>
2025-08-19 17:04:55 -0300

    Setting the color tone variants of the buttonIcon component

--- todo-list/src/components/button-icon.tsx
+++ todo-list/src/components/button-icon.tsx
@@ -4,7 +4,15 @@ import { cva } from "class-variance-authority";
 
 export const buttonIconVariants = cva(`
   inline-flex items-center justify-center cursor-pointer transition group
-`)
+`, {
+  variants: {
+    variant: {
+      primary: "bg-green-base hover:bg-green-dark",
+      secondary: "bg-pink-base hover:bg-pink-dark",
+      tertiary: "bg-transparent hover:bg-gray-200"
+    }
+  }
+})
 
 interface ButtonIconProps extends Omit<React.ComponentProps<"button">, "size" | "disabled"> {
   icon: React.ComponentProps<typeof Icon>["svg"]
