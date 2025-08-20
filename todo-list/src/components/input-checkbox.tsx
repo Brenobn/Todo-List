dcdc60581dba396ebf947fa4d6be3730d54aebae
Lucas Veloso <lucas.g-external@intelltech.com>
2025-08-20 13:56:27 -0300

    Creanting a const named inputCheckboxIconVariants using cva to style it

--- todo-list/src/components/input-checkbox.tsx
+++ todo-list/src/components/input-checkbox.tsx
@@ -30,3 +30,20 @@ export const inputCheckboxVariants = cva(
   }
 );
 
+export const inputCheckboxIconVariants = cva(
+  `
+    absolute top-1/2 left-1 -translate-y-1/2
+    hidden peer-checked:block fill-white 
+  `,
+  {
+    variants: {
+      size: {
+        md: "w-3 h-3"
+      }
+    },
+    defaultVariants: {
+      size: "md"
+    }
+  }
+)
+
