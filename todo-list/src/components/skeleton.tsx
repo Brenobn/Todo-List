1bb963e9ef84ccabbc3f952d1309bb3468b3644a
Lucas Veloso <lucas.g-external@intelltech.com>
2025-08-20 17:05:14 -0300

    Finishing the skeleton component

--- todo-list/src/components/skeleton.tsx
+++ todo-list/src/components/skeleton.tsx
@@ -21,3 +21,16 @@ export const skeletonVariants =  cva(
 interface SkeletonProps extends VariantProps<typeof skeletonVariants>,
   React.ComponentProps<"div"> {}
 
+export default function Skeleton({
+  rounded,
+  className,
+  ...props
+}: SkeletonProps) {
+  return (
+    <div 
+      className={skeletonVariants({rounded, className})} 
+      {...props}
+    />
+  );
+}
+
