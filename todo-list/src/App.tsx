56a728e458330962eb90f183c77d857b66b3e7a9
Brenobn <brenobragabn@gmail.com>
2025-08-21 11:11:15 -0300

    Importing BtowserRouter, Route, Routes from react-router and also pagecomponents from pages

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import PageComponents from "./pages/page-components";
 
 
 export default function App() {
-  
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/componentes" element={<PageComponents />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
