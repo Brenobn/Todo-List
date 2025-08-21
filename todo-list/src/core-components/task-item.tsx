262782346eb178c9f3bf1dfdfedbe607a51345a5
Brenobn <brenobragabn@gmail.com>
2025-08-21 16:33:16 -0300

    Importing useState and creating a state for isEditing, and handleEditTask, handleExitEditTask functions

--- todo-list/src/core-components/task-item.tsx
+++ todo-list/src/core-components/task-item.tsx
@@ -4,17 +4,51 @@ import InputCheckbox from "../components/input-checkbox";
 import Text from "../components/text";
 import TrashIcon from "../assets/icons/Trash.svg?react";
 import PencilIcon from "../assets/icons/Pencil.svg?react";
+import CheckIcon from "../assets/icons/Check.svg?react";
+import XIcon from "../assets/icons/X.svg?react";
+import React from "react";
+import InputText from "../components/input-text";
 
 
 export default function TaskItem() {
+  const [isEditing, setIsEditing] = React.useState(false);
+
+  function handleEditTask() {
+    setIsEditing(true);
+  }
+
+  function handleExitEditTask() {
+    setIsEditing(false);
+  }
+
   return(
     <Card size="md" className="flex items-center gap-4">
-      <InputCheckbox />
-      <Text className="flex-1">🛒 Fazer compras da semana</Text>
-      <div className="flex gap-1">
-        <ButtonIcon icon={TrashIcon} variant="tertiary" />
-        <ButtonIcon icon={PencilIcon} variant="tertiary" />
-      </div>
+      {!isEditing ? 
+        <>
+          <InputCheckbox />
+          <Text className="flex-1">🛒 Fazer compras da semana</Text>
+          <div className="flex gap-1">
+            <ButtonIcon icon={TrashIcon} variant="tertiary" />
+            <ButtonIcon 
+              icon={PencilIcon} 
+              variant="tertiary" 
+              onClick={handleEditTask}
+            />
+          </div>
+        </>
+        :
+        <>
+          <InputText className="flex-1" />
+          <div className="flex gap-1">
+            <ButtonIcon 
+              icon={XIcon} 
+              variant="secondary"
+              onClick={handleExitEditTask} 
+            />
+            <ButtonIcon icon={CheckIcon} variant="primary" />
+          </div>
+        </>
+      }
     </Card>
   );
 }
