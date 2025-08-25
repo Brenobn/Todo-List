e79d2f79579543261eb4ccf1300df5037636e6a8
Brenobn <brenobragabn@gmail.com>
2025-08-25 13:58:05 -0300

    fixing the import from use-task

--- todo-list/src/core-components/tasks-list.tsx
+++ todo-list/src/core-components/tasks-list.tsx
@@ -1,7 +1,7 @@
 import Button from "../components/button";
 import PlusIcon from "../assets/icons/Plus.svg?react";
 import TaskItem from "./task-item";
-import useTasks from "../hooks/use-tasks";
+import useTasks from "../hooks/use-task";
 
 export default function TasksList() {
   const { tasks } = useTasks();
